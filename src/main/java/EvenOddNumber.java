b796ccc240a7bb202df396716ec1ee1ab1175f47
Nikitha-1418 <nikithacheera25@gmail.com>
2024-10-28 14:25:21 +0530

    EvenOddNumber.java

--- src/main/java/EvenOddNumber.java
+++ src/main/java/EvenOddNumber.java
@@ -5,25 +5,25 @@ public class EvenOddNumber {
 
    
     public String defineEvenNumber() {
-        return "An even number is an integer that is exactly divisible by 2.";
+        return "even number.";
     }
 
-    // Method to define an odd number
+    
     public String defineOddNumber() {
-        return "An odd number is an integer that is not exactly divisible by 2.";
+        return "odd number.";
     }
 
-    // Method to verify if a number is even or odd
+    
     public String verifyIfNumberIsEvenOrOdd(int num) {
         // Check for negative numbers
         if (num < 0) {
             return "-1";
         }
-        // Check for zero
+       
         if (num == 0) {
             return "-2";
         }
-        // Determine if the number is even or odd
+       
         if (num % 2 == 0) {
             return "Even Number";
         } else {
