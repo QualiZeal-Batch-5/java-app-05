0efe803dd7f8e0e1051c1322223e1017aaf78099
Naga Nishitha Devi <nishithadevi789@gmail.com>
2024-10-28 14:22:57 +0530

    Solution

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+
+import com.qualizeal.javaapp05.operation.EvenOddNumber;
+
+public class App {
+    public static void main(String[] args) {
+        EvenOddNumber evenOddNumber = new EvenOddNumber();
+
+        evenOddNumber.verifyIfNumberIsEvenOrOdd(10);
+	}
+}
---
+++ src/main/java/EvenOddNumber.java
@@ -0,0 +1,28 @@
+// File: EvenOddNumber.java
+// Package declaration
+package com.qualizeal.javaapp05.operation;
+
+public class EvenOddNumber {
+
+    public String defineEvenNumber() {
+        return "An even number is an integer which is divisible by 2.";
+    }
+
+    public String defineOddNumber() {
+        return "An odd number is an integer which is not divisible by 2.";
+    }
+
+    public String verifyIfNumberIsEvenOrOdd(int number) {
+        if (number < 0) {
+            return "-1";
+        }
+        if (number == 0) {
+            return "-2";
+        }
+        if (number % 2 == 0) {
+            return "Even Number";
+        } else {
+            return "Odd Number";
+        }
+    }
+}
