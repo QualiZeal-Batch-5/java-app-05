1d1e6d440913bfdb98bc0813d55fead575973bb1
Ramya Mudragalla <mudragallaramya28@gmail.com>
2024-10-28 15:02:44 +0530

    EvenOdd

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
-
+import com.qualizeal.javaapp05.operation.EvenOddNumber;
 public class App {
 	public static void main(String[] args) {
+		EvenOddNumber evenOddNumber = new EvenOddNumber();
+		String result = evenOddNumber.verifyIfNumberIsEvenOrOdd(11);
+		
+		System.out.println(result);
 		
 	}
 }
