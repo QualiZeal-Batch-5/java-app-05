f102c345a58cab49a1fcb631691bc5f7c76a0876
Bhavya Reddy <karribhavyareddy6@gmail.com>
2024-10-28 14:59:43 +0530

    Added Solution.

---
+++ src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
@@ -0,0 +1,28 @@
+package com.qualizeal.javaapp05.operation;
+
+public class EvenOddNumber {
+	
+	public String defineEvenNumber() {
+		return "Any number that can be exactly divisible by 2.";
+	}
+	
+	public String defineOddNumber() {
+		return "Any number that cannot be exactly divisible by 2.";
+	}
+	
+	public String verifyIfNumberIsEvenOrOdd(int num1) {
+		if(num1 < 0) {
+			return "-1";
+		}
+		else if(num1 == 0) {
+			return "-2";
+		}
+		else if(num1 % 2 == 0) {
+			return "Even Number";
+		}	
+		else {
+			return "Odd Number";
+		}
+	}
+}
+
