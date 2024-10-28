6cf0cd173255e3c85c90a2093270b692145f9e42
Lakavath Savithri <lakavath.savithri@qualizeal.com>
2024-10-28 15:36:42 +0530

    added solution

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,14 @@
 package com.qualizeal;
-
+import com.qualizeal.javaapp05.operation.EvenOddNumber;
 public class App {
 	public static void main(String[] args) {
 		
+		EvenOddNumber evenOddNumber = new EvenOddNumber();
+		
+			System.out.println(evenOddNumber.verifyIfNumberIsEvenOrOdd(5));
+			System.out.println(evenOddNumber.verifyIfNumberIsEvenOrOdd(8)); 
+			System.out.println(evenOddNumber.verifyIfNumberIsEvenOrOdd(-9)); 
+			System.out.println(evenOddNumber.verifyIfNumberIsEvenOrOdd(0));
+			
 	}
 }
