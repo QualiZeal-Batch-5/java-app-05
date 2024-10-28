eaea16e7fc6311ee8b028fac0dabec53cd7df0e8
Madhusri <polamurimadhusri363@gmail.com>
2024-10-28 14:59:30 +0530

    Solution to EvenOddNumber

--- src/main/java/EvenOddNumber.java
+++ src/main/java/EvenOddNumber.java
@@ -1,18 +1,22 @@
 package com.qualizeal.javaapp05.operation;
 
 public class EvenOddNumber {
+	public String defineEvenNumber() {
+		return "Even Number";
+	}
+
+	public String defineOddNumber() {
+		return "Odd Number";
+	}
+
 	public String verifyIfNumberIsEvenOrOdd(int num1) {
-		if(num1<0) {
-			return "-1";
-		}
-		else if(num1==0) {
+		if (num1 == 0) {
 			return "-2";
+		} else if (num1 < 0) {
+			return "-1";
+		} else if (num1 % 2 == 0) {
+			return defineEvenNumber();
 		}
-		else if(num1%2 == 0) {
-			return "Even Number";
-		}	
-		else{
-			return "Odd Number";
-		}	
+		return defineOddNumber();
 	}
 }
