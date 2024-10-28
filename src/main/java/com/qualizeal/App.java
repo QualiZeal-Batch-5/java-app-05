d581ae961c3e55dee7ce4896627ae9fbd7237cf1
Poloju Lahari <poloju.lahari@qualizeal.com>
2024-10-28 15:24:42 +0530

    EvenOddNumbersCode

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,17 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp05.operation.EvenOddNumber;
+
 public class App {
 	public static void main(String[] args) {
+		EvenOddNumber evenOddNumber  = new EvenOddNumber();
+		evenOddNumber.defineEvenNumber();
+		evenOddNumber.defineOddNumber();
+		evenOddNumber.verifyIfNumberIsEvenOrOdd(6);
 		
-	}
+		int input =  6;
+		String result = evenOddNumber.verifyIfNumberIsEvenOrOdd(input);
+		System.out.println(result);
+	}		
 }
+
