15710ca282f24036d59243a1c32f5e873e7aa6a5
Kattupalli Bhushana kumari <bhushana1805@gmail.com>
2024-10-28 15:51:07 +0530

    Performed an operation to identify whether a number is EVEN or ODD.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,18 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp05.operation.EvenOddNumber;
+
 public class App {
 	public static void main(String[] args) {
+		EvenOddNumber IsEvenOrOdd = new EvenOddNumber();
+		String res1 = IsEvenOrOdd.verifyIfNumberIsEvenOrOdd(2);
+		String res2 = IsEvenOrOdd.verifyIfNumberIsEvenOrOdd(3);
+		String res3 = IsEvenOrOdd.verifyIfNumberIsEvenOrOdd(-5);
+		String res4 = IsEvenOrOdd.verifyIfNumberIsEvenOrOdd(0);
 		
+		System.out.println(res1);
+		System.out.println(res2);
+		System.out.println(res3);
+		System.out.println(res4);
 	}
 }
--- src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
+++ src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
@@ -3,29 +3,31 @@ package com.qualizeal.javaapp05.operation;
 public class EvenOddNumber {
 	
 	public String defineEvenNumber() {
-        return "An even number is an integer that is exactly divisible by 2.";
+		return "Even Number";
 	}
 	
-	
 	public String defineOddNumber() {
-        return "An odd number is an integer that is not exactly divisible by 2.";
-    }
-	
+		return "Odd Number";
+	}
 	
 	public String verifyIfNumberIsEvenOrOdd(int number) {
+		String answer = " ";
 		
-        if (number < 0) {
-            return "-1"; 
-        }
-        if (number == 0) {
-            return "-2"; 
-        }
-        
-        if (number % 2 == 0) {
-            return "Even Number"; 
-        } 
-        else {
-            return "Odd Number"; 
-        }
-    }
+		if(number % 2 == 0) {
+			answer = defineEvenNumber();
+		}
+		
+		if(number % 2 != 0) {
+			answer = defineOddNumber();
+		}
+		
+		if(number < 0) {
+			answer = "-1";
+		}
+		
+		if(number == 0) {
+			answer = "-2";
+		}
+		return answer;
+	}
 }
