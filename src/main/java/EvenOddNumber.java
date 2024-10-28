bd67975bc5e24345cc2a096e633ca48c098a2940
chandini <chandinimalla3@gmail.com>
2024-10-28 14:21:33 +0530

    Solution added

---
+++ src/main/java/EvenOddNumber.java
@@ -0,0 +1,29 @@
+package com.qualizeal.javaapp05.operation;
+
+public class EvenOddNumber{
+		public String defineEvenNumber(){
+			return "Even Number";
+
+		}
+		
+		public String defineOddNumber(){
+			return "Odd Number";
+
+		}
+		
+		public String verifyIfNumberIsEvenOrOdd(int num1){
+			if(num1 < 0){
+				return "-1";
+			}
+			else if(num1 == 0){
+				return "-2";
+			}
+			else if(num1 % 2 == 0){
+				return defineEvenNumber();
+			}
+			else{
+				return defineOddNumber();
+			}
+		}	
+		
+}
--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,10 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp05.operation.EvenOddNumber;
+
 public class App {
 	public static void main(String[] args) {
+		EvenOddNumber evenOddNumber=new EvenOddNumber();
 		
 	}
 }
