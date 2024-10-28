2e941c73c21fa32f853052ccef3139ed1a04d2d0
Purna <purnaande2004@gmail.com>
2024-10-28 15:10:52 +0530

    Solution Added

--- src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
+++ src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
@@ -3,12 +3,12 @@ package com.qualizeal.javaapp05.operation;
 public class EvenOddNumber {
 	
 	public String defineEvenNumber() {
-        return "An even number is an integer that is exactly divisible by 2.";
+        return "An even number is an integer that is divisible by 2.";
 	}
 	
 	
 	public String defineOddNumber() {
-        return "An odd number is an integer that is not exactly divisible by 2.";
+        return "An odd number is an integer that is not divisible by 2.";
     }
 	
 	
