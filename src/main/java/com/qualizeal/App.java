3c2ec23fd9fa4c01c26a46af92ff25ed5a520104
Swarna Modepalli <swarnamodepalli954@gmail.com>
2024-10-28 15:03:01 +0530

    EvenOdd

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,13 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp05.operation.EvenOddNumber;
+
 public class App {
 	public static void main(String[] args) {
+		
+		EvenOddNumber evenOddNumber = new EvenOddNumber();
+		String result = evenOddNumber.verifyIfNumberIsEvenOrOdd(5);
+		System.out.println(result);
 		
 	}
 }
--- src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
+++ src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
@@ -3,25 +3,25 @@ package com.qualizeal.javaapp05.operation;
 public class EvenOddNumber {
 	
 	public String defineEvenNumber() {
-        return "An even number is an integer that is exactly divisible by 2.";
+        return "Any number that can be exactly divisible by 2.";
 	}
 	
 	
 	public String defineOddNumber() {
-        return "An odd number is an integer that is not exactly divisible by 2.";
+        return "Any number that cannot be exactly divisible by 2.";
     }
 	
 	
-	public String verifyIfNumberIsEvenOrOdd(int number) {
+	public String verifyIfNumberIsEvenOrOdd(int num) {
 		
-        if (number < 0) {
+        if (num < 0) {
             return "-1"; 
         }
-        if (number == 0) {
+        if (num == 0) {
             return "-2"; 
         }
         
-        if (number % 2 == 0) {
+        if (num % 2 == 0) {
             return "Even Number"; 
         } 
         else {
