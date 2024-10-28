11c92eba44ccb8f9ae95086e72efdd793691efa7
SnehareddyPasham <snehalathareddy3002@gmail.com>
2024-10-28 19:07:12 +0530

    assignment added

--- src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
+++ src/main/java/com/qualizeal/javaapp05/operation/EvenOddNumber.java
@@ -3,29 +3,37 @@ package com.qualizeal.javaapp05.operation;
 public class EvenOddNumber {
 	
 	public String defineEvenNumber() {
-        return "An even number is an integer that is exactly divisible by 2.";
+        
+		return "An even number is an integer that is exactly divisible by 2.";
 	}
 	
 	
 	public String defineOddNumber() {
-        return "An odd number is an integer that is not exactly divisible by 2.";
+        
+		return "An odd number is an integer that is not exactly divisible by 2.";
     }
 	
 	
-	public String verifyIfNumberIsEvenOrOdd(int number) {
+	public String verifyIfNumberIsEvenOrOdd(int num) {
 		
-        if (number < 0) {
-            return "-1"; 
-        }
-        if (number == 0) {
-            return "-2"; 
+        if (num < 0) {
+            
+        	return "-1"; 
         }
         
-        if (number % 2 == 0) {
-            return "Even Number"; 
+        else if (num == 0) {
+            
+             return "-2"; 
+        }
+        
+        else if (num % 2 == 0) {
+            
+        	return "Even Number"; 
         } 
+        
         else {
-            return "Odd Number"; 
+            
+        	return "Odd Number"; 
         }
     }
 }
